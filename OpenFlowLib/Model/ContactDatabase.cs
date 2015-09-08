c3aedfc52f06cca838ecb7ee52437682f67013ec
Henning Thoele <hthoele@gmail.com>
2015-09-08 09:40:12 +0200

    Added Couchbase DB

--- OpenFlowLib/Model/ContactDatabase.cs
+++ OpenFlowLib/Model/ContactDatabase.cs
@@ -1,58 +1,87 @@
 ﻿using System;
 using System.Collections.Generic;
 using MimeKit;
+using Couchbase.Lite;
 
 namespace OpenFlowLib.Model
 {
 	public static class ContactDatabase
 	{
-		private static Dictionary<string, Contact> knownContacts;
+		private static Database database;
+		private static Dictionary<string, string> emailLookup;
 
 		static ContactDatabase ()
 		{
-			knownContacts = new Dictionary<string, Contact> ();
+			database = Manager.SharedInstance.GetDatabase ("Contacts");
+			emailLookup = new Dictionary<string, string> ();
 		}
 
 		public static int Count
 		{
 			get
 			{
-				return knownContacts.Count;
+				return database.DocumentCount;
 			}
 		}
 
-		public static Contact GetContact(string eMailAddress)
+		private static string GetDatabaseId(string eMailAddress)
 		{
 			string key = eMailAddress.ToLowerInvariant ();
-			if (knownContacts.ContainsKey (key))
-				return knownContacts [key];
+			string docId = null;
+			if (emailLookup.ContainsKey (key))
+			{
+				return emailLookup [key];
+			}
+
+			if (docId == null)
+			{
+				// TODO: Lookup in database
+			}
 
 			return null;
 		}
 
+		public static Contact GetContact(string eMailAddress)
+		{
+			string docId = GetDatabaseId (eMailAddress);
+			Document doc = database.GetExistingDocument (docId);
+			if (doc == null)
+				return null;
+
+			Dictionary<String, Object> newProperties = new Dictionary<String, Object>(doc.Properties);
+			return newProperties["contact"] as Contact;
+		}
+
 		public static Contact AddContact(string setLocalName, MailboxAddress setAddress)
 		{
-			string key = setAddress.Address.ToLowerInvariant ();
+			string docId = GetDatabaseId (setAddress.Address);
+			Document doc = database.GetExistingDocument (docId);
 
-			if (knownContacts.ContainsKey (key))
+			if (doc != null)
 			{
-				Contact contact = knownContacts [key];
+				Dictionary<String, Object> newProperties = new Dictionary<String, Object>(doc.Properties);
+				Contact contact = newProperties["contact"] as Contact;
 				contact.Address = setAddress;
 				contact.LocalName = setLocalName;
+				doc.PutProperties (newProperties);
 				return contact;
 			} else
 			{
 				Contact newContact = new Contact () { LocalName = setLocalName, Address = setAddress };
-				knownContacts.Add (key, newContact);
+				doc = database.CreateDocument ();
+				Dictionary<string, object> vals = new Dictionary<string, object> { { "contact", newContact } };
+				doc.PutProperties (vals);
+				emailLookup.Add (setAddress.Address, doc.Id);
 				return newContact;
 			}
 		}
 
-		public static void RemoveContact(string eMailAdress)
+		public static void RemoveContact(string eMailAddress)
 		{
-			string key = eMailAdress.ToLowerInvariant ();
-			if (knownContacts.ContainsKey (key))
-				knownContacts.Remove (key);
+			string docId = GetDatabaseId (eMailAddress);
+			Document doc = database.GetExistingDocument (docId);
+			if (doc != null)
+				doc.Delete ();
 		}
 	}
 }
